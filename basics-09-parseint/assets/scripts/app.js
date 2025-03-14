fada8242f1ef9dbcba4a1db077aac02370330ca9
Tigran Tovmasyan <fobostt@yandex.ru>
2025-03-14 15:50:23 +0300

    random stuff 2

--- basics-09-parseint/assets/scripts/app.js
+++ basics-09-parseint/assets/scripts/app.js
@@ -1,57 +1,77 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function getUserNumInput(){
-  return parseInt(userInput.value)
+function getUserNumInput() {
+  return parseInt(userInput.value);
 }
 
-function resultDescription(operator, resBeforeCalc, calcNum){
+function resultDescription(operator, resBeforeCalc, calcNum) {
   const description = `${resBeforeCalc} ${operator} ${calcNum}`;
   outputResult(currentResult, description);
 }
 
+function writeToLog(operID, initRes, userNum, outRes) {
+  const logEntry = {
+    operation: operID,
+    prevRes: initRes,
+    number: userNum,
+    result: outRes,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
 
 function add() {
   const enteredNum = getUserNumInput();
   const initialRes = currentResult;
   currentResult += enteredNum;
-  resultDescription('+', initialRes, enteredNum);
+  resultDescription("+", initialRes, enteredNum);
+  writeToLog("add", initialRes, enteredNum, currentResult);
 }
-
 
 function multiply() {
   const enteredNum = getUserNumInput();
   const initialRes = currentResult;
   currentResult *= enteredNum;
-  resultDescription('*', initialRes, enteredNum);
+  resultDescription("*", initialRes, enteredNum);
+  writeToLog("mult", initialRes, enteredNum, currentResult);
 }
-
 
 function divide() {
   const enteredNum = getUserNumInput();
   const initialRes = currentResult;
-    if(enteredNum == 0){
-      alert("pls don't divide by zero");
-      } 
-    else {
+  if (enteredNum == 0) {
+    alert("pls don't divide by zero");
+  } else {
     currentResult /= enteredNum;
-    resultDescription('/', initialRes, enteredNum);
-    }
+    resultDescription("/", initialRes, enteredNum);
+    writeToLog("division", initialRes, enteredNum, currentResult);
+  }
 }
-
 
 function subtract() {
   const enteredNum = getUserNumInput();
   const initialRes = currentResult;
   currentResult -= enteredNum;
-  resultDescription('-', initialRes, enteredNum);
+  resultDescription("-", initialRes, enteredNum);
+  writeToLog("sub", initialRes, enteredNum, currentResult);
 }
 
 function modulo() {
   const enteredNum = getUserNumInput();
   const initialRes = currentResult;
   currentResult %= enteredNum;
-  resultDescription('%', initialRes, enteredNum);
+  resultDescription("%", initialRes, enteredNum);
+  writeToLog("modulo", initialRes, enteredNum, currentResult);
+}
+
+function sqrt() {
+  const enteredNum = getUserNumInput();
+  const initialRes = currentResult;
+  currentResult = initialRes * initialRes;
+  resultDescription("^2", initialRes, enteredNum);
+  writeToLog("sqrt", initialRes, enteredNum, currentResult);
 }
 
 multiplyBtn.addEventListener("click", multiply);
@@ -59,4 +79,4 @@ divideBtn.addEventListener("click", divide);
 subtractBtn.addEventListener("click", subtract);
 addBtn.addEventListener("click", add);
 moduloBtn.addEventListener("click", modulo);
-
+sqrtBtn.addEventListener("click", modulo);
